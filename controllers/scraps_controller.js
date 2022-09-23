a2999cb36f6154225aea9192068277ff1689126f
hyunjung <hhhj.1486@gmail.com>
2022-09-24 01:09:42 +0900

    Refactor: 오타수정

--- controllers/scraps_controller.js
+++ controllers/scraps_controller.js
@@ -8,7 +8,7 @@ const addPostScraps = async(req, res) => {
     const params = {token, post_id};
     await scrapsService.addPostScraps(params);
 
-    return res.status(201).json({ message: 'scrap add succes' });
+    return res.status(201).json({ message: 'scrap add success' });
   } catch (err) {
     return res.status(err.statusCode || 500).json({ message: err.message });
   }
@@ -22,7 +22,7 @@ const deletePostScraps = async(req, res) => {
     const params = {token, post_id};
     await scrapsService.deletePostScraps(params);
 
-    return res.status(200).json({ message: 'scrap delete succes' });
+    return res.status(200).json({ message: 'scrap delete success' });
   } catch (err) {
     return res.status(err.statusCode || 500).json({ message: err.message });
   }
