129a5621eebca5dff0841d89974ea969d11a70bc
kimuni98 <88352859+kimuni98@users.noreply.github.com>
2022-09-27 17:25:58 +0900

    Fix : 쿼리 조건 수정

--- models/ranking_dao.js
+++ models/ranking_dao.js
@@ -63,8 +63,10 @@ const getEditorChoice = async ()=>{
     JOIN users u ON u.unique_id = p.user_id
     JOIN user_scores us ON us.user_id = p.user_id 
     JOIN sub_category sub ON p.sub_category_id = sub.unique_id
+    JOIN main_category mc ON p.main_category_id = mc.unique_id 
     LEFT JOIN post_tags pt ON p.unique_id = pt.post_id
     LEFT JOIN tags t ON pt.tage_id = t.unique_id 
+    WHERE mc.unique_id = 1 OR mc.unique_id = 2
     GROUP BY p.unique_id
     ORDER BY ANY_VALUE(us.score) DESC
     LIMIT 0, 5;`
@@ -107,9 +109,11 @@ const getWeeklyBest = async ()=>{
     JOIN users u ON u.unique_id = p.user_id
     JOIN user_scores us ON us.user_id = p.user_id 
     JOIN sub_category sub ON p.sub_category_id = sub.unique_id
+    JOIN main_category mc ON p.main_category_id = mc.unique_id 
     LEFT JOIN post_tags pt ON p.unique_id = pt.post_id
     LEFT JOIN tags t ON pt.tage_id = t.unique_id 
     WHERE ANY_VALUE(p.create_at) BETWEEN DATE_ADD(NOW(),INTERVAL -1 WEEK ) AND NOW()
+    AND mc.unique_id = 1 OR mc.unique_id = 2
     GROUP BY p.unique_id
     ORDER BY recommend_cnt DESC
     LIMIT 0, 5;`
